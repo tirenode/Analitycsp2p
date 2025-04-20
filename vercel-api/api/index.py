9ee88dc4e34eb6ed3ad38a082a0e529fef7ced08
diego calvo <41440204-diegocalvo070@users.noreply.replit.com>
2025-04-20 13:27:20 +0000

    Update index.py handler with JSON response

--- vercel-api/api/index.py
+++ vercel-api/api/index.py
@@ -1,6 +1,9 @@
 
+import json
+
 def handler(request):
     return {
         "statusCode": 200,
-        "body": "¡Bienvenido a AnalitycsP2P API en Vercel!"
+        "headers": {"Content-Type": "application/json"},
+        "body": json.dumps({"message": "¡Bienvenido a AnalitycsP2P API en Vercel!"})
     }
